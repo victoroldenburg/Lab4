9c2bf4d1bca01b3294144b502af26348c5609ae0
victoroldenburg <60806635+victoroldenburg@users.noreply.github.com>
2020-03-30 19:23:48 +0200

    finished BFS_shortest path

--- Project1/SSC.c
+++ Project1/SSC.c
@@ -48,11 +48,6 @@ int runEx3SCC() {
 	printArray(graph_ex3);
 
 	BFS(graph_ex3, vertex1);
-	BFS(graph_ex3, vertex2);
-	BFS(graph_ex3, vertex3);
-	BFS(graph_ex3, vertex4);
-	BFS(graph_ex3, vertex5);
-	BFS(graph_ex3, vertex6);
 
 	//#####################Free Memory############################
 	freeMemory(graph_ex3);
